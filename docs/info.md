488aa2e896ebd12acd34a98fbeaf932c7a317128
Lukas Baumann <lukas_baumann@ggs.ch>
2024-09-04 07:39:17 +0200

    Initial commit of layout

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,9 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
+Analog building-blocks for Tiny Tapeout 8:
+- Bandgap
+- Ref. Current Source
+- LDO
+- Ring Oscillator
 
-## How to test
-
-Explain how to use your project
-
-## External hardware
-
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
--- src/project.v
+++ src/project.v
@@ -5,10 +5,10 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_devstdin_LDO_OSC (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
-//    input  wire       VAPWR,    // 3.3v power supply
+    input  wire       VAPWR,    // 3.3v power supply
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
